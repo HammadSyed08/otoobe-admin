f4e6052db5d4cb38075f139fccdee421852a2572
Hammad Syed <hammadword08@gmail.com>
2025-09-05 12:07:05 +0500

    Direct use Keys

--- lib/auth-context.tsx
+++ lib/auth-context.tsx
@@ -7,7 +7,7 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import { auth } from "./firebase";
+import { auth } from "@/lib/firebase";
 import { useToast } from "@/hooks/use-toast";
 
 interface AuthContextType {
